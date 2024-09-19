13f33028eec7687797c61349a85c35b3055f1388
Blossom <saphashb@gmail.com>
2024-09-19 13:24:38 +0100

    set up project assets

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Shater's icon</h1>
+      <Link href="#">About me</Link>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import localFont  from 'next/font/local';
+
+export const mena = localFont({
+  src: './fonts/Mena Grotesk Regular.woff2',
+  display: 'swap',
+  variable: '--font-mena'
+})
