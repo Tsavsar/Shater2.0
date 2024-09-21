eac07f755fab80a11b3dc9954a85f79870a02c1c
Blossom <saphashb@gmail.com>
2024-09-21 13:32:02 +0100

    customize footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,14 +5,16 @@ const Footer = () => {
   let Dev;
 
   return (
-    <div className='pt-80'>
+    <div className='pt-40'>
+      <div className="responsive f-satoshi">
       <div className="flex justify-between">
-        <a href='mailto:shatermt@gmail.com'>shatermt@gmail.com</a>
-        <div>
-          <p>Designed By Shater</p>
-          <p>Built By {Dev}</p>
+        <a className='text-[#9A9A9A] font-light text-xs' href='mailto:shatermt@gmail.com'>shatermt@gmail.com</a>
+        <div className='flex gap-2'>
+          <p className='gradient-text font-light text-xs'>Designed By Shater</p>
+          <p className='text-[#9A9A9A] font-light text-xs'>Built By {Dev}</p>
         </div>
-        <p>© 2024 Shater M. Tsavsar</p>
+        <p className='text-[#9A9A9A] font-light text-xs'>© 2024 Shater M. Tsavsar</p>
+      </div>
       </div>
     <Image src="/images/footer-desktop.svg" alt="Shatermt.com" width={2416} height={278} />
     </div>
