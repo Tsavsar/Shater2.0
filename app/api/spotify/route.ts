39d30a9788941e9885119267a7d6492a31455213
blossom-babs <saphashb@gmail.com>
2024-11-09 21:14:58 +0100

    update image modal

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -7,19 +7,16 @@ export async function GET() {
     useCache: false,
     cacheDuration: 30000,
     streamerArgs: {
-      clientId: process.env.WSPOTIFY_CLIENT_ID || 'FOO',
-      clientSecret: process.env.WSPOTIFY_CLIENT_SECRET || 'BAR',
-      refreshToken: process.env.WSPOTIFY_REFRESH_TOKEN || 'BAZ',
+      clientId: process.env.SPOTIFY_CLIENT_ID || 'FOO',
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || 'BAR',
+      refreshToken: process.env.SPOTIFY_REFRESH_TOKEN || 'BAZ',
     },
   });
 
   try {
     const data = await np.fetchCurrentlyPlayingOrLastPlayed();
-    
-    console.log(data)
     return Response.json({ data });
   } catch (error) {
-    console.log(error)
     return Response.json({ error});
   }
 }
