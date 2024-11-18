6d653654ae7379a567c7d7d705d3e9f2fb6adb39
blossom-babs <saphashb@gmail.com>
2024-11-18 18:01:29 +0100

    use shater's spotify creds

---
+++ app/now-playing/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NowPlaying from '../components/NowPlaying'
+
+const page = () => {
+  return (
+    <div className='w-[60%] mx-auto mt-[30vh]'>
+        <div>
+          <NowPlaying/>
+        </div>
+    </div>
+  )
+}
+
+export default page
