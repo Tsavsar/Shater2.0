3d7578e439a1ab1d0362d2d3af66125fbc7adb5e
Haniel Ubogu <hanieltobi@gmail.com>
2024-12-24 14:48:57 +0000

    toggle menu

--- app/components/NavDropdown.tsx
+++ app/components/NavDropdown.tsx
@@ -1,73 +1,64 @@
-'use client';
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image';
-import { dropdownList } from '../utils';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { dropdownList } from "../utils";
 
-interface INavDropdown{
-  defineActiveItem: (e:string) => void;
+interface INavDropdown {
+  defineActiveItem: (e: string) => void;
 }
 
-export const NavDropdownContainer:React.FC<INavDropdown> = ({defineActiveItem}) => {
-  const [mobile, setMobile] = useState(false)
-  const [desktop, setDesktop] = useState(false)
-  const [activeItem, setActiveItem] = useState('projects')
+export const NavDropdownContainer: React.FC<INavDropdown> = ({ defineActiveItem }) => {
+  const [mobile, setMobile] = useState(true);
+  const [desktop, setDesktop] = useState(true);
+  const [activeItem, setActiveItem] = useState("projects");
 
   const handleTextChange = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const text = e.currentTarget.innerText.toLowerCase()
-    setActiveItem(text)
-    setMobile(false)
-    setDesktop(false)
+    const text = e.currentTarget.innerText.toLowerCase();
+    setActiveItem(text);
+    setMobile(false);
+    setDesktop(false);
     //defineActiveItem(text)
-  }
-
-  const handleMobileLeave = () => {
-    setMobile(false)
-  }
-  const handleMobileEnter = () => {
-    setMobile(true)
-  }
-  const handleDesktopLeave = () => {
-    setDesktop(false)
-  }
-  const handleDesktopEnter = () => {
-    setDesktop(true)
-  }
-  const handleMobile = () => {
-    setMobile(!mobile)
-  }
+  };
 
   useEffect(() => {
-    defineActiveItem(activeItem)
-  }, [activeItem, defineActiveItem])
-  
+    defineActiveItem(activeItem);
+  }, [activeItem, defineActiveItem]);
+
   useEffect(() => {
-      defineActiveItem(dropdownList[0].toLowerCase())
-      setActiveItem(dropdownList[0].toLowerCase())
-  }, [defineActiveItem])
+    defineActiveItem(dropdownList[0].toLowerCase());
+    setActiveItem(dropdownList[0].toLowerCase());
+  }, [defineActiveItem]);
 
-  return(
-    <NavDropdown handleMobile={handleMobile} handleTextChange={handleTextChange} desktop={desktop} activeItem={activeItem} handleDesktopEnter={handleDesktopEnter} handleDesktopLeave={handleDesktopLeave} handleMobileLeave={handleMobileLeave} handleMobileEnter={handleMobileEnter} mobile={mobile}/>
-  )
-}
+  const toggleMenu = () => {
+    setMobile(!mobile);
+    setDesktop(!desktop);
+  };
 
-interface NavDropdownProps {
-  handleMobileLeave: () => void;  
-  handleMobileEnter: () => void;  
-  handleDesktopLeave: () => void;  
-  handleDesktopEnter: () => void;  
-  handleMobile: () => void;  
-  handleTextChange: (e:React.MouseEvent<HTMLButtonElement>) => void;  
-  mobile: boolean;              
-  desktop: boolean;              
-  activeItem: string          
+  return (
+    <NavDropdown
+      handleTextChange={handleTextChange}
+      desktop={desktop}
+      activeItem={activeItem}
+      mobile={mobile}
+      toggleMenu={toggleMenu}
+    />
+  );
 };
 
-const NavDropdown: React.FC<NavDropdownProps> = (props) => {
+interface NavDropdownProps {
+  handleTextChange: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  mobile: boolean;
+  desktop: boolean;
+  activeItem: string;
+  toggleMenu: () => void;
+}
+
+const NavDropdown: React.FC<NavDropdownProps> = props => {
   useEffect(() => {
     const title = document.querySelectorAll(".title-slide");
 
     const RowObserver = new IntersectionObserver(
-      (entries) =>
+      entries =>
         entries.forEach((entry, index) => {
           const target = entry.target as HTMLElement;
 
@@ -82,80 +73,84 @@ const NavDropdown: React.FC<NavDropdownProps> = (props) => {
       { threshold: 0.1 }
     );
 
-    title.forEach((el) => {
+    title.forEach(el => {
       RowObserver.observe(el);
     });
 
     return () => {
-      title.forEach((el) => {
+      title.forEach(el => {
         RowObserver.unobserve(el);
       });
     };
   }, []);
 
-
   return (
     <div className="flex pb-10 items-center gap-4">
-    <div className="c-menu">
-      <>
-      <svg className="svg-area" width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-    <g transform="translate(8,8)"> 
-      <circle id="radar" cx="0" cy="0" r="6" /> 
-        <circle id="core" cx="0" cy="0" r="4" />        
-      
-    
-    </g>
-</svg>
-      </>
-      <span className="f-satoshi font-medium text-sm text-[#19170E]">Menu</span>
-    </div>
-    <hr className="slant-divider" />
-    {/* mobile -onMouseLeave={props.handleMobileLeave} onMouseEnter={props.handleMobileEnter} */}
-    
-    <div onClick={props.handleMobile}   className="md:hidden cursor-pointer  relative">
-    <div  className="capitalize dropdown-active flex items-center gap-4">
-      {props.activeItem}
-      <Image
-        className='mobile-dropdown'
-        src="/images/mobile-dropdown.svg"
-        alt="Dropdown"
-        width={10}
-        height={10}
-      />
-      <div className={`${props.mobile ? 'grid' : 'hidden'} absolute -left-8 -top-24`}>
-     <div className='z-30 dropdown-slide rounded-xl grid p-4 px-8 dropdown-mobile cursor-pointer mt-[8rem] bg-[#F9F8F6] border-[#F5F3F0] border gap-4'>
-     {dropdownList.filter(item => item.toLowerCase() !== props.activeItem).map(item => (
-      <button key={item} onClick={props.handleTextChange} className='capitalize py-1 text-left hover:opacity-70'>{item}</button>
-     ))}
-     </div>
+      <div className="c-menu">
+        <>
+          <svg className="svg-area" width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(8,8)">
+              <circle id="radar" cx="0" cy="0" r="6" />
+              <circle id="core" cx="0" cy="0" r="4" />
+            </g>
+          </svg>
+        </>
+        <span className="f-satoshi font-medium text-sm text-[#19170E]">Menu</span>
       </div>
-    </div>
-    {/* grid */}
-  </div>
-  {/* desktop */}
-    <div className="hidden md:flex">
-    <div  onMouseLeave={props.handleDesktopLeave} onMouseEnter={props.handleDesktopEnter}  className="relative cursor-pointer capitalize dropdown-active flex items-center gap-4">
-    <span>{props.activeItem}</span>
-      <Image
-        className=''
-        src={props.desktop ? '/images/active.svg' : '/images/dropdown.svg'}
-        alt="Dropdown"
-        width={props.desktop ? 5 : 10}
-        height={props.desktop ? 5 :10}
-      />
-      <div className={`${props.desktop ? 'flex' : 'hidden'} dropdown-children items-center gap-4`}>
-      {dropdownList.filter(item => item.toLowerCase() !== props.activeItem).map(item => (
-        <button key={item} onClick={props.handleTextChange} className='cop-dropdown-link hover:opacity-70'>{item}</button>
+      <hr className="slant-divider" />
+      {/* mobile -onMouseLeave={props.handleMobileLeave} onMouseEnter={props.handleMobileEnter} */}
 
-      ))}
-      {/* <a className='dropdown-link' href="#">Education</a>
+      <div onClick={props.toggleMenu} className="md:hidden cursor-pointer  relative">
+        <div className="capitalize dropdown-active flex items-center gap-4">
+          {props.activeItem}
+          <Image className="mobile-dropdown" src="/images/mobile-dropdown.svg" alt="Dropdown" width={10} height={10} />
+          <div className={`${props.mobile ? "grid" : "hidden"} absolute -left-8 -top-24`}>
+            <div className="z-30 dropdown-slide rounded-xl grid p-4 px-8 dropdown-mobile cursor-pointer mt-[8rem] bg-[#F9F8F6] border-[#F5F3F0] border gap-4">
+              {dropdownList
+                .filter(item => item.toLowerCase() !== props.activeItem)
+                .map(item => (
+                  <button
+                    key={item}
+                    onClick={props.handleTextChange}
+                    className="capitalize py-1 text-left hover:opacity-70"
+                  >
+                    {item}
+                  </button>
+                ))}
+            </div>
+          </div>
+        </div>
+        {/* grid */}
+      </div>
+      {/* desktop */}
+      <div className="hidden md:flex">
+        <div
+          onClick={props.toggleMenu}
+          className="relative cursor-pointer capitalize dropdown-active flex items-center gap-4"
+        >
+          <span>{props.activeItem}</span>
+          <Image
+            className=""
+            src={props.desktop ? "/images/active.svg" : "/images/dropdown.svg"}
+            alt="Dropdown"
+            width={props.desktop ? 5 : 10}
+            height={props.desktop ? 5 : 10}
+          />
+          <div className={`${props.desktop ? "flex" : "hidden"} dropdown-children items-center gap-4`}>
+            {dropdownList
+              .filter(item => item.toLowerCase() !== props.activeItem)
+              .map(item => (
+                <button key={item} onClick={props.handleTextChange} className="cop-dropdown-link hover:opacity-70">
+                  {item}
+                </button>
+              ))}
+            {/* <a className='dropdown-link' href="#">Education</a>
       <a className='dropdown-link' href="#">Recommendation</a> */}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-    
-  </div>
-  )
-}
+  );
+};
 
-export default NavDropdown
+export default NavDropdown;
