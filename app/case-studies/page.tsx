c658445aea538222b7e91acf4d8c6b0a6a79d787
Ahumareze <ahumarezeifeanyi001@gmail.com>
2025-01-26 10:18:04 +0100

    setup case studies page structure

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Loader from "../components/general/Loader";
+import Navbar from "../components/Navbar";
+
 export default function page(){
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLoading(false);
+        }, 500); // Simulate a 3-second loading time
+        return () => clearTimeout(timer);
+    }, []);
+
+    if(loading) return <Loader pageName="Case-studies" />
+
     return(
-        <>
-        
-        </>
+        <section className="responsive pt-16">
+            <Navbar navItl="Home" navLink="/" />
+                <main className={`pt-16`}>
+
+                </main>
+        </section>
     )
 }
